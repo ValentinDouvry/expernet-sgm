a1a1f34d6a62a504f715adbd350a1da3105a1fae
Dylan <dylan.domen@gmail.com>
2020-11-22 12:00:30 +0100

    Code de connexion avec la db "expernetsgm" qui est ma base test. on changera avec la vrai base si le nom n'est pas le même.

---
+++ connect_db.php
@@ -0,0 +1,14 @@
+<?php
+
+    $host = 'localhost';
+    $dbname = 'expernetsgm';
+    $user = 'root';
+    $pass = '';
+
+
+    $db = new PDO("mysql:host=$host;dbname=$dbname",$user,$pass,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+
+    if($db == false){
+        echo "erreur lors de la connexion avec la base";
+    }
+?>
