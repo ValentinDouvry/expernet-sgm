3b27f6123d35a7e10e0733bfeca98c4f6dd5261c
Douvry <valentin.jabba@gmail.com>
2020-11-23 13:56:08 +0400

    Merge ?

--- index.php
+++ index.php
@@ -2,19 +2,11 @@
    session_start();
    
    if(isset($_SESSION['userConnected'])) {
-<<<<<<< Updated upstream
        header('Location: views/list_group.php');
    }
 
    else {
        header('Location: views/log_in.php');
-=======
-       header('Location: /expernet-sgm/views/list_group');
-   }
-
-   else {
-       header('Location: /expernet-sgm/views/log_in.php');
->>>>>>> Stashed changes
    }
    
 ?>
--- views/log_in.php
+++ views/log_in.php
@@ -17,11 +17,7 @@
     ?>
 
     <div class="container">
-<<<<<<< Updated upstream
-    <form method = 'POST' action="../actions/user_connexion.php" class="was-validated">
-=======
     <form method = 'POST' action="../secret_connect.php" class="was-validated">
->>>>>>> Stashed changes
         <div class="form-group">
         <label for="login">Identifiant</label>
         <input type="text" class="form-control" id="login" placeholder="Entrer votre identifiant" name="login" required>
