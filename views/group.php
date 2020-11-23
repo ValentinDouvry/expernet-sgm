51b2fd324332d5ab69ac00759e6c1f27c223ff69
Dylan <dylan.domen@gmail.com>
2020-11-23 13:25:24 +0100

    Modif du code de la page group
    -affichage du nom du groupe de l'utilisateur
    -affichage en card des membres du groupe(avatar,nom,prénom,pseudo,level)

--- views/group.php
+++ views/group.php
@@ -10,65 +10,76 @@
 <body>
     <?php
     session_start();
-    include ("secret/connect_db.php");
+    include("../secret/connect_db.php");
+    include("../classes/User.php");
+    include("../classes/Group.php");
         
     if(!isset($_SESSION['userId'])){
         header('Location:log_in.php');
     }else{
         $userId = $_SESSION['userId'];
+        //requete pour récuperer le id du groupe dans lequel est l'utilisateur
         $query = $db->prepare("SELECT groupId FROM users where id = :id");
-        $query->bindParam(":id",$userId);
+        $query->bindParam(":id",$userId,PDO::PARAM_INT);
         $query->execute();
-        $query->setFetchMode(PDO::FETCH_ASSOC);
-        $groupId = $query->fetch();
-        
-        var_dump($groupId);
-
-        echo $groupId['groupId'];
-
+        $query->setFetchMode(PDO::FETCH_CLASS,'User');
+        $data = $query->fetch();
+        $groupId = $data->getGroupId();
+ 
+        //requete pour recupérer le nom du groupe associer à l'id du groupe récupérer juste avant
         $query = $db->prepare("SELECT name FROM groups where id = :id");
-        $query->bindParam(":id",$groupId['groupId']);
+        $query->bindParam(":id",$groupId);
         $query->execute();
-        $query->setFetchMode(PDO::FETCH_ASSOC);
-        $groupName = $query->fetch();
-
-        var_dump($groupName);
-
+        $query->setFetchMode(PDO::FETCH_CLASS,'Group');
+        $data = $query->fetch();
+        $groupName = $data->getName();
     ?>
 
-    <div class="container">
+    <div class="container-fluid">
         <?php
-            // $groupeName = $data[''];
-            // echo"<h1>".$groupeName."</h1>";
+            echo"<h1>".$groupName."</h1>";
         ?>
         
         <!-- Content here -->
         <?php
-            // $query = $db->prepare("SELECT * FROM users where groupeId = :groupeId");
-            // $query->bindParam(":groupeId",$); 
-            // $query->execute();
-            // $query->setFetchMode(PDO::FETCH_ASSOC);
-            // $data = $query->fetch();
+            $query = $db->prepare("SELECT * FROM users where groupId = :groupId");
+            $query->bindParam(":groupId",$groupId); 
+            $query->execute();
+            $query->setFetchMode(PDO::FETCH_CLASS,'User');
+            $data = $query->fetchall();
+
+            foreach($data as $user){
         ?>
-        <div class="card mb-3" style="max-width: 540px;">
-            <div class="row no-gutters">
-                <div class="col-md-4">
-                <img src="..." class="card-img" alt="...">
-                </div>
-                <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                </div>
+        <div class = "row">
+            <div class="card mb-3 col-sm-9" style="max-width: 540px;">
+                <div class="row no-gutters">
+                    <div class="col-md-4">
+                    <?php
+                        $avatarId = $user->getAvatarId();
+                        $query = $db->prepare("SELECT imageName FROM avatars where id = :id");
+                        $query->bindParam(":id",$avatarId); 
+                        $query->execute();
+                        $query->setFetchMode(PDO::FETCH_ASSOC);
+                        $data = $query->fetch();
+                        $imageName = $data['imageName'];
+                    ?>
+                    <img src="<?php $imageName ?>" class="card-img" alt="<?php echo $imageName ?>">
+                    </div>
+                    <div class="col-md-4">
+                    <div class="card-body">
+                        <h5 class="card-title"><?php echo $user->getLastName()." ",$user->getName(); ?></h5>
+                        <p class="card-text"><?php echo $user->getUsername();?></p>
+                        <p class="card-text"><small class="text-muted"><?php echo"Level : ".$user->getLevel();?></small></p>
+                    </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
     <?php
+            } 
     }
-    ?>
-
+    ?>   
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
