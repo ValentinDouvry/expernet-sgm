440cd3392e28b48ca1d13a50381d60b222bb5e12
Douvry <valentin.jabba@gmail.com>
2020-11-23 13:54:16 +0400

    Auto stash before merge of "main" and "origin/main"

--- views/log_in.php
+++ views/log_in.php
@@ -17,7 +17,11 @@
     ?>
 
     <div class="container">
+<<<<<<< Updated upstream
     <form method = 'POST' action="../actions/user_connexion.php" class="was-validated">
+=======
+    <form method = 'POST' action="../secret_connect.php" class="was-validated">
+>>>>>>> Stashed changes
         <div class="form-group">
         <label for="login">Identifiant</label>
         <input type="text" class="form-control" id="login" placeholder="Entrer votre identifiant" name="login" required>
