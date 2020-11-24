d8e2d83082bcae3242b328d6d2fdcdf6b4bb2548
David <dadahuet13@gmail.com>
2020-11-24 15:59:21 +0400

    Supprime toute les occurences de l'items dans la bases  de donnes

--- actions/item_delete.php
+++ actions/item_delete.php
@@ -1,3 +1,47 @@
 <?php
 
-var_dump($_GET);
+require_once('../secret/connect_db.php');
+require_once('../classes/User.php');
+session_start();
+
+if(!isset($_SESSION['userId'])){
+    header('Location:../views/log_in.php');
+    exit;
+} 
+
+$sql = "SELECT * FROM users WHERE id=?";
+$query = $db->prepare($sql);
+$query->execute(array($_SESSION['userId']));
+$user = $query->fetchObject('User');
+
+if(!$user->getIsAdmin()){
+    header('Location:../views/shop.php?status=danger&text=pas admin');
+    exit;
+} 
+
+if(!isset($_GET['id'])) {
+    header('Location:../views/shop.php?status=danger&text=pas d\'objet selectionner');
+    exit;
+}
+
+$sql = "DELETE FROM `inventories` WHERE `itemId`=?";
+$query = $db->prepare($sql);
+$is_success = $query->execute(array($_GET['id']));
+
+if(!$is_success) {
+    header('Location: ../views/shop.php?status=danger&text=Une erreur est survenu, veillez réesseyer');
+    exit();
+}
+
+
+$sql = "DELETE FROM `items` WHERE `id`=?";
+$query = $db->prepare($sql);
+$is_success = $query->execute(array($_GET['id']));
+
+if($is_success) {
+    header('Location: ../views/shop.php?status=success&text=l\'objet a était corectement ');
+    exit();
+} else {
+    header('Location: ../views/shop.php?status=danger&text=Une erreur est survenu, veillez réesseyer');
+    exit();
+}
