2471ff97a43c96ea787998b31fec849de555443f
David <dadahuet13@gmail.com>
2020-11-27 18:52:00 +0400

    creation d'un fichier script pour les avatars

---
+++ js/show_avatar.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+    const canvas = document.getElementById('canvasAvatar');
+    context = canvas.getContext('2d');
+
+
+    function loadImages(sources, callback) {
+        var images = {};
+        var loadedImages = 0;
+        var numImages = 0;
+        // get num of sources
+        for (var src in sources) {
+            numImages++;
+        }
+        for (var src in sources) {
+            images[src] = new Image();
+            images[src].onload = function() {
+                if (++loadedImages >= numImages) {
+                    callback(images);
+                }
+            };
+            images[src].src = sources[src];
+        }
+    }
+
+    loadImages(phpsource, function(images) {
+        for (const property in phpsource) {
+            if (property == "base") {
+                context.drawImage(images.base, 25, 50, 200, 200);
+            }
+
+            if (property == "hat") {
+                context.drawImage(images.hat, 35, -30, 180, 180);
+            }
+
+            if (property == "glase") {
+                context.drawImage(images.glase, 75, 70, 100, 100);
+            }
+        }
+
+    });
+
+});
